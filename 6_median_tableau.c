7c51cb7defbef002cf3852e7fca258f4c8877c72
ramseslekeumo-create <ramseslekeumo@gmail.com>
2025-09-25 10:18:08 +0100

    Update 6_median_tableau.c

--- 6_median_tableau.c
+++ 6_median_tableau.c
@@ -1,47 +1,48 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define TAILLE_MAX 500
-
-int main() {
-    int n;
-    float x;
-    float T[TAILLE_MAX];
-
-    printf("Entrer le bnombre d'élémetns du tableau (un entier).\n");
-    do
-    {
-        scanf("%d", &n);
-    } while (n<=0 && n>TAILLE_MAX);
-
-    printf("Entrer les éléments du tableaux:\n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%f", &T[i]);
-    }
-    
-    //Trie
-    for (int i = 0; i < n-1; i++)
-    {
-        for (int j = 0; j < n - i-1; j++)
-        {  
-            if (T[j+1] < T[j])
-            {
-                x = T[j];
-                T[j] = T[j+1];
-                T[j+1] = x;
+void trier_tableau(int tab[] , int taille ) {
+int i , j , temps ;
+   for (i=0 ; i<= taille-1 ; i++ ) {
+        for (j= i+1 ; j<taille ; j++ ) {
+             if (tab[i] > tab[j]) {
+             temps = tab[i] ;
+             tab[i] = tab[j] ;
+             tab[j] = temps ;
             }
-            
         }
     }
-
-    if (0 == n%2)
-    {
-        printf("\tLa mediane est : %.2f.\n", (T[n/2-1] + T[n/2])/2);
-    } else 
-    {
-        printf("\tLa mediane est : %.2f.\n", T[n/2]);
-    }
-
-    return 0;
+ }
+ 
+ // fonction calculer_median
+ double calculer_median( int tab[] , int taille ) {
+       if(taille % 2 == 0 ){
+       return (tab[(taille / 2)-1] + tab[taille / 2]) / 2.0 ;
+       } else {
+            return tab[taille / 2 ] ;
+       }
+ }
+  
+  int main() {
+    int taille ;
+    printf("entrer la taille du tableau:") ;
+    scanf("%d" , &taille ) ;
     
-}
+    int tab[taille] ;
+    int i ;
+    printf("entrer les elements du tableau:") ;
+    for (i=0 ; i<taille ; i++ ) {
+         scanf("%d" , &tab[i]) ;
+ }
+ 
+trier_tableau(tab, taille ) ;	
+
+float median = calculer_median(tab, taille ) ;
+printf("le median est : %2f\n " , median ) ;
+
+return 0;
+
+} 
+
+      
+
